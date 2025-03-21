aa07013fbbd4be18cfda066028a9370727902cd0
Swapnasheela12 <swapnasheela.study@gmail.com>
2025-03-21 15:54:24 -0400

    toolbar update

--- src/app/main_module/home/home.component.ts
+++ src/app/main_module/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../common_content/footer/footer.component";
+import { HeaderComponent } from "../../common_content/header/header.component";
+import { MyServicesComponent } from "../../common_content/my-services/my-services.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MyServicesComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

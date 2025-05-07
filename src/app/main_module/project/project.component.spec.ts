83ba53b2693921f21af907fd163e7566866099cc
Swapnasheela12 <swapnasheela.study@gmail.com>
2025-05-07 14:03:17 -0400

    project code

--- src/app/main_module/project/project.component.spec.ts
+++ src/app/main_module/project/project.component.spec.ts
@@ -1,4 +1,7 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { ProjectComponent } from './project.component';
 
@@ -6,12 +9,14 @@ describe('ProjectComponent', () => {
   let component: ProjectComponent;
   let fixture: ComponentFixture<ProjectComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProjectComponent]
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProjectComponent ]
     })
     .compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ProjectComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();

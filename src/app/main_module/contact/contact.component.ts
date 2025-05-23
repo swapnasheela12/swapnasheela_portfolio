5cc594f92d857b29589d44932e0c44fd2815cd79
Swapnasheela12 <swapnasheela.study@gmail.com>
2025-05-22 23:28:37 -0400

    changes fix firebase

--- src/app/main_module/contact/contact.component.ts
+++ src/app/main_module/contact/contact.component.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ThreeDTravelStoryComponent } from './threeDTravelStory/threeDTravelStory.component';
 import { TravelStoryComponent } from './travel-story/travel-story.component';
-
 @Component({
   selector: 'app-contact',
-  imports: [FormsModule, GoogleMapsModule, TravelStoryComponent],
+  imports: [FormsModule, GoogleMapsModule, TravelStoryComponent, MatSnackBarModule],
   standalone: true,
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+
   title = 'google-maps-demo';
 
   // Set the center of the map (latitude, longitude)
@@ -23,10 +27,15 @@ export class ContactComponent {
   // Set a marker on the map
   markerPosition = { lat: 40.31, lng: -74.32 }; // San Francisco
   contact = { name: '', email: '', message: '' };
-
+  constructor(public snackBar: MatSnackBar) { }
   sendMessage() {
-    console.log("Message Sent:", this.contact);
-    alert("Your message has been sent!");
+    this.snackBar.open('Message sent', 'Close', {
+      duration: 3000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top'
+    });
+
     this.contact = { name: '', email: '', message: '' };
+
   }
 }
